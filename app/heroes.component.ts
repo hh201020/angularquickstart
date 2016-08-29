2d45eca84f0245e456150a165bd013ec435381a5
Jerry Hu <hh201020@gmail.com>
2016-08-29 15:41:30 -0400

    7 Http with promise.

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -7,26 +7,49 @@ import { Router } from '@angular/router';
 @Component({
   selector: `my-heroes`,
   templateUrl: 'app/heroes.component.html',
-  styleUrls:  ['app/heroes.component.css']
+  styleUrls: ['app/heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
   title = 'Tour of Heroes';
+  heroes: Hero[];
   selectedHero: Hero;
 
   constructor(private router: Router,
     private heroService: HeroService) { }
 
-  heroes: Hero[];
   ngOnInit(): void {
     this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+  // getHeroes(): void {
+  //   this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  // }
   getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService
+      .getHeroes()
+      .then(heroes => this.heroes = heroes);
   }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
+
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
   }
