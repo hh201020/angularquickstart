59edcb43c81fca4fc7df6dc696c8a6beb89fc3b2
Jerry Hu <hh201020@gmail.com>
2017-02-19 12:01:28 -0500

    Make product name a link, passing parameters to a route

--- PS_Angular2GettingStarted/app/products/product-detail.component.ts
+++ PS_Angular2GettingStarted/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -8,4 +9,12 @@ import { IProduct } from './product';
 export class ProductDetailComponent {
     pageTitle: string = 'Product Detail';
     product: IProduct;
+
+    constructor(private _route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
 }
