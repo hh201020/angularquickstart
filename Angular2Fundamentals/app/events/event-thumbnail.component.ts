889c2e5fb6d9b9aaab97b0560a2467f20033ce85
Jerry Hu <hh201020@gmail.com>
2017-02-20 16:43:24 -0500

    styling components with ngStyle / ngClass

--- Angular2Fundamentals/app/events/event-thumbnail.component.ts
+++ Angular2Fundamentals/app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
-      <div>Time: {{event?.time}}</div>
+      <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{event?.price}}</div>
       <div *ngIf="event?.location">
         <span>Location: {{event?.location?.address}}</span>
@@ -32,6 +37,12 @@ export class EventThumbnailComponent {
   handleClickMe() {
     console.log('clicked!');
     this.eventClick.emit('from output: ' + this.event.name);
+  }
+  
+  getStartTimeStyle() {
+    if (this.event && this.event.time === '8:00 am')
+      return {color: '#003300', 'font-weight': 'bold'}
+    return {}
   }
 
   logFoo() {
