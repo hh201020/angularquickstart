c836691bc37437f5d499dfd503b189847efd45f6
Jerry Hu <hh201020@gmail.com>
2017-02-20 12:37:25 -0500

    one way data binding by {{ event.name }}

--- Angular2Fundamentals/app/events/events-list.component.ts
+++ Angular2Fundamentals/app/events/events-list.component.ts
@@ -5,8 +5,23 @@ import { Component } from '@angular/core'
   template: `
   <div>
     <h1>Upcoming Angular 2 Events</h1>
+    <hr>
+    <h2>{{event.name}}</h2>
   </div>
   `
 })
 export class EventsListComponent {
+  event = {
+      id: 1,
+      name: 'Angular Connect',
+      date: '9/26/2036',
+      time: '10:00 am',
+      price: 599.99,
+      imageUrl: '/app/assets/images/angularconnect-shield.png',
+      location: {
+        address: '1057 DT',
+        city: 'London',
+        country: 'England'
+      }
+  }
 }
