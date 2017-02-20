dfef6846c5a6c62cd816b22f2ff3deb244bf5c1c
Jerry Hu <hh201020@gmail.com>
2017-02-20 16:24:48 -0500

    &ngIf to comment out items to make Angular not render it

--- Angular2Fundamentals/app/events/event-thumbnail.component.ts
+++ Angular2Fundamentals/app/events/event-thumbnail.component.ts
@@ -8,18 +8,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
       <div>Date: {{event?.date}}</div>
       <div>Time: {{event?.time}}</div>
       <div>Price: \${{event?.price}}</div>
-      <div>
+      <div *ngIf="event?.location">
         <span>Location: {{event?.location?.address}}</span>
         <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
       </div>
-      <div>
+      <div *ngIf="event?.onlineUrl">
         Online URL: {{event?.onlineUrl}}
       </div>
       <button class="btn btn-primary" (click)="handleClickMe()">Click Me!</button>
     </div>
   `,
   styles: [`
-    .thumbnail { min-height: 300px; }
+    .thumbnail { min-height: 220px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
   `]
