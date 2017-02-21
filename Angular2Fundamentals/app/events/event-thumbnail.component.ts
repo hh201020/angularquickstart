7173af6071242f6cad044bb95db48b63393df463
Jerry Hu <hh201020@gmail.com>
2017-02-21 11:29:49 -0500

    turn div into a route:  [routerLink]="['/events', event.id]"

--- Angular2Fundamentals/app/events/event-thumbnail.component.ts
+++ Angular2Fundamentals/app/events/event-thumbnail.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 @Component({
   selector: 'event-thumbnail',
   template: `
-    <div class="well hoverwell thumbnail">
+    <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
       <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
@@ -38,7 +38,7 @@ export class EventThumbnailComponent {
     console.log('clicked!');
     this.eventClick.emit('from output: ' + this.event.name);
   }
-  
+
   getStartTimeStyle() {
     if (this.event && this.event.time === '8:00 am')
       return {color: '#003300', 'font-weight': 'bold'}
