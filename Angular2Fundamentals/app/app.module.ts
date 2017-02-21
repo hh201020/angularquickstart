6cc6900ae9a56c5cbb52693b470df051f46b533b
Jerry Hu <hh201020@gmail.com>
2017-02-21 13:27:38 -0500

    Organizing exports with barrels

--- Angular2Fundamentals/app/app.module.ts
+++ Angular2Fundamentals/app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule, ActivatedRouteSnapshot } from '@angular/router'
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component'
-import { EventsListComponent } from './events/events-list.component'
-import { CreateEventComponent } from './events/create-event.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
-import { NavBarComponent } from './nav/navbar.component'
-import { EventService } from './events/shared/event.service'
-import { ToastrService } from './common/toastr.service'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
-import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component'
-import { EventRouteActivator } from './events/event-details/event-route-activator.service'
-import { EventListResolver } from './events/events-list-resolver.service'
+import { NavBarComponent } from './nav/navbar.component'
+import { ToastrService } from './common/toastr.service'
+import { appRoutes } from './routes'
 
 @NgModule({
   imports: [
