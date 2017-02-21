fc23ac794bd858ed93a2750d1fa38d55b97b2f6b
Jerry Hu <hh201020@gmail.com>
2017-02-21 14:49:23 -0500

    basic login page by http://localhost:8808/user/login

---
+++ Angular2Fundamentals/app/user/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
+
+@Component({
+  templateUrl: 'app/user/login.component.html',
+  styles: [`
+    em { float:right; color:#E05C65; padding-left:10px; }
+  `]
+})
+export class LoginComponent {
+  constructor(private router:Router) {
+
+  }
+
+}
