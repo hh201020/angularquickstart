f535e67c3b25c9e7b27b1e058cb6f9b8f520da48
Jerry Hu <hh201020@gmail.com>
2017-02-22 14:40:40 -0500

    OpaqueToken for dependency injection to solve third party global services problem

--- Angular2Fundamentals/app/events/events-list.component.ts
+++ Angular2Fundamentals/app/events/events-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service'
-import { ToastrService } from '../common/toastr.service'
 import { ActivatedRoute } from '@angular/router'
 import { IEvent } from './shared/event.model'
 
@@ -12,7 +11,6 @@ import { IEvent } from './shared/event.model'
     <div class="row">
       <div *ngFor="let event of events" class="col-md-5">
         <event-thumbnail 
-          (click)="handleThumbnailClick(event.name)" 
           [event]="event" >
         </event-thumbnail>
       </div>
@@ -23,7 +21,7 @@ import { IEvent } from './shared/event.model'
 export class EventsListComponent  implements OnInit {
   events:IEvent[]
 
-  constructor(private eventService: EventService,  private toastr: ToastrService, private route:ActivatedRoute) {
+  constructor(private eventService: EventService, private route:ActivatedRoute) {
   }
 
   ngOnInit() {
@@ -31,7 +29,4 @@ export class EventsListComponent  implements OnInit {
                                                       // which gives getEvents from EventListResolver.resolve()
   }
 
-  handleThumbnailClick(eventName){
-    this.toastr.success(eventName);
-  }
 }
