0f63e4c9a9f2a1c7733c095f669adc0740438c20
Jerry Hu <hh201020@gmail.com>
2017-02-23 15:52:37 -0500

    Using observable to get event details, but the old method canActivate is still working, that should be changed.

--- Angular2Fundamentals/app/events/event-details/event-details.component.ts
+++ Angular2Fundamentals/app/events/event-details/event-details.component.ts
@@ -21,8 +21,10 @@ export class EventDetailsComponent {
 
   ngOnInit() {
     this.route.params.forEach((params: Params) => {
-      this.event = this.eventService.getEvent(+params['id']);
-      this.addMode = false;
+      this.eventService.getEvent(+params['id']).subscribe((event: IEvent) => {
+        this.event = event;
+        this.addMode = false;
+      })
     })
   }
 
--- Angular2Fundamentals/app/events/shared/event.service.ts
+++ Angular2Fundamentals/app/events/shared/event.service.ts
@@ -14,8 +14,10 @@ export class EventService {
     }).catch(this.handleError);
   }
 
-  getEvent(id:number):IEvent {
-    return EVENTS.find(event => event.id === id)
+  getEvent(id:number):Observable<IEvent> {
+    return this.http.get("/api/events/" + id).map((response: Response) => {
+        return <IEvent>response.json();
+    }).catch(this.handleError);
   }
 
   saveEvent(event) {
