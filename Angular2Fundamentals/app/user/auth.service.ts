e6edb96318de91ecbb8517e98ea5eb0ed50231f1
Jerry Hu <hh201020@gmail.com>
2017-02-24 13:29:07 -0500

    checking user login status when refreshing page, this api is not good, it returns only the same user.

--- Angular2Fundamentals/app/user/auth.service.ts
+++ Angular2Fundamentals/app/user/auth.service.ts
@@ -1,34 +1,50 @@
 import { Injectable } from '@angular/core'
 import { IUser } from './user.model'
-import { Http, Response, Headers, RequestOptions} from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class AuthService {
-  currentUser:IUser
+  currentUser: IUser
 
-  constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
   loginUser(userName: string, password: string) {
-    let headers = new Headers({ 'Content-Type': 'application/json'});
-    let options = new RequestOptions({headers: headers});
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
     let loginInfo = { username: userName, password: password };
 
     return this.http.post('/api/login', JSON.stringify(loginInfo), options)
-    .do(resp => {
-      if(resp) {
-        this.currentUser = <IUser>resp.json().user;
-      }
-    }).catch(error => {
-      return Observable.of(false);
-    })
+      .do(resp => {
+        if (resp) {
+          this.currentUser = <IUser>resp.json().user;
+        }
+      }).catch(error => {
+        return Observable.of(false);
+      })
   }
 
   isAuthenticated() {
     return !!this.currentUser;
   }
 
-  updateCurrentUser(firstName:string, lastName:string) {
+  checkAuthenticationStatus() {
+    return this.http.get('/api/currentIdentity').map((response: any) => {
+      if (response._body) {
+        return response.json();
+      } else {
+        return {}
+      }
+    })
+      .do(currentUser => {
+        if (!!currentUser.userName) {
+          this.currentUser = currentUser;
+        }
+      })
+      .subscribe();
+  }
+
+  updateCurrentUser(firstName: string, lastName: string) {
     this.currentUser.firstName = firstName
     this.currentUser.lastName = lastName
   }
