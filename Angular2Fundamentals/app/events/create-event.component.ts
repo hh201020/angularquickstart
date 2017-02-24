566de1b3b45b709477b73a1d8bd8d876251372ed
Jerry Hu <hh201020@gmail.com>
2017-02-24 11:53:25 -0500

    Post method to save event

--- Angular2Fundamentals/app/events/create-event.component.ts
+++ Angular2Fundamentals/app/events/create-event.component.ts
@@ -20,9 +20,10 @@ export class CreateEventComponent {
   constructor(private router: Router, private eventService:EventService) {}
 
   saveEvent(formValues) {
-    this.eventService.saveEvent(formValues)
+   this.eventService.saveEvent(formValues).subscribe(event => {
+    this.router.navigate(['/events']) 
     this.isDirty = false
-    this.router.navigate(['/events'])
+   })
   }
 
   cancel() {
