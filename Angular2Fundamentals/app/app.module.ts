fd28c18364e4fecc4bf0f5604003128720e6781a
Jerry Hu <hh201020@gmail.com>
2017-02-25 11:28:35 -0500

    tslint needs to be installed globally to work, and it needs also install typescript globally:
    npm install -g tslint typescript

--- Angular2Fundamentals/app/app.module.ts
+++ Angular2Fundamentals/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { RouterModule, ActivatedRouteSnapshot } from '@angular/router'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { HttpModule } from '@angular/http'
+import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, ActivatedRouteSnapshot } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import {
   EventsListComponent,
@@ -18,7 +18,7 @@ import {
   VoterService,
   LocationValidator,
   DurationPipe
-} from './events/index'
+} from './events/index';
 
 import { JQ_TOKEN,
   TOASTR_TOKEN, 
@@ -26,15 +26,15 @@ import { JQ_TOKEN,
   CollapsibleWellComponent,
   SimpleModalComponent,
   ModalTriggerDirective 
-} from './common/index'
+} from './common/index';
 
-import { EventsAppComponent } from './events-app.component'
-import { NavBarComponent } from './nav/navbar.component'
-import { appRoutes } from './routes'
-import { Error404Component } from './errors/404.component'
-import { AuthService } from './user/auth.service'
+import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
+import { AuthService } from './user/auth.service';
 
-declare let toastr : Toastr
+declare let toastr : Toastr;
 declare let jQuery : Object;
 
 @NgModule({
@@ -81,6 +81,6 @@ export class AppModule { }
 
 function checkDirtyState(component: CreateEventComponent) {
   if (component.isDirty)
-    return window.confirm('You have not saved this event, do you really want to cancel?')
-  return true
+    return window.confirm('You have not saved this event, do you really want to cancel?');
+  return true;
 }
