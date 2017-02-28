133408245484216e353d80fe36bc33ef56de5179
Jerry Hu <hh201020@gmail.com>
2017-02-28 15:16:00 -0500

    extract video.js

--- PS_ElectronFundamentals/03-capture-video/src/capture.js
+++ PS_ElectronFundamentals/03-capture-video/src/capture.js
@@ -1,12 +1,4 @@
-navigator.getUserMedia = navigator.webkitGetUserMedia
-
-function handleSuccess(videoEl, stream) {
-  video.src = window.URL.createObjectURL(stream)
-}
-
-function handleError(error) {
-  console.log('Camera error', error)
-}
+const video = require('./video')
 
 window.addEventListener('DOMContentLoaded', _ => {
   const videoEl = document.getElementById('video')
@@ -16,18 +8,7 @@ window.addEventListener('DOMContentLoaded', _ => {
   const counterEl = document.getElementById('counter')
   const flashEl = document.getElementById('flash')
 
-  const constraints = {
-    audio: false,
-    video: {
-      madatory:{
-        minWidth: 853,
-        minHeight: 480,
-        maxWidth: 853,
-        maxHeight: 480
-      }
-    }
-  }
+  video.init(navigator, videoEl)
 
-  navigator.getUserMedia(constraints, stream => handleSuccess(videoEl, stream), handleError)
 })
 
