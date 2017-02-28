86ae35fa85e8f91ad6bd6527c6b2ca70da42e8f1
Jerry Hu <hh201020@gmail.com>
2017-02-28 12:25:34 -0500

    Clipboard Buffer 01: console show stack

--- PS_ElectronFundamentals/02-app-demo/src/main.js
+++ PS_ElectronFundamentals/02-app-demo/src/main.js
@@ -2,21 +2,37 @@ const electron = require('electron')
 const path = require('path')
 
 const app = electron.app
+const clipboard = electron.clipboard
 const Menu = electron.Menu
 const Tray = electron.Tray
 
-app.on('ready', _ => {
-  const tray = new Tray(path.join('src', 'trayIcon.png'))
-  const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'Wow',
-      click: _ => console.log('wow')
-    },
-    {
-      label: 'Quit',
-      click: _ => app.quit()
+const ITEM_MAX_LENGTH = 20
+const STACK_SIZE = 5
+
+
+function addToStack(item, stack = []) {
+  return [item].concat(stack.length >= STACK_SIZE ? stack.slice(0, stack.length - 1) : stack)
+}
+
+function checkClipboardForChange(clipboard, onChange) {
+  let cache = clipboard.readText()
+  let latest
+  setInterval(_ => {
+    latest = clipboard.readText()
+    if (latest !== cache) {
+      cache = latest
+      onChange(cache)
     }
-  ])
-  tray.setToolTip('Woweeee')
-  tray.setContextMenu(contextMenu)
+  }, 1000)
+}
+
+app.on('ready', _ => {
+  let stack = []
+  const tray = new Tray(path.join('src', 'trayIcon.png'))
+  tray.setContextMenu(Menu.buildFromTemplate([{ label: '<Empty>', enabled: false }]))
+
+  checkClipboardForChange(clipboard, text => {
+    stack = addToStack(text, stack)
+    console.log("stack: ", stack)
+  })
 })
